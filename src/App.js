da6288233049b00718f2bda14c6213ffc8869f3d
To Nok Hang, Tommy <55172561+nhto@users.noreply.github.com>
2022-04-26 14:58:18 +0800

    title update

--- src/App.js
+++ src/App.js
@@ -10,35 +10,40 @@ import TopbarPhoto from './topbar_photo.jpg';
 
 
 class App extends Component {
-	  useEffect(() => {       
-	  document.title = `testing project name`;  
-	  });
-  render() {  
-    return (
-	<BrowserRouter>
-	
-	<div  style={{
-		background: `url(${TopbarPhoto})`,
-		}}
-	>
-	<h3 style ={{color: "black"}}>Tommy To's personal website</h3>
-	<ul>
-	<p className = "p1"><Link to="/">Home</Link>&nbsp;
-	<Link to="/resume">Resume</Link>&nbsp;
-	<Link to="/project">Project</Link>&nbsp;
-	<Link to="/contact">Contact</Link>
-	</p>
-	</ul>
-	
-	<Route exact path="/" component={Home} />
-	<Route path="/resume" component={Resume} />
-	<Route path="/project" component={Project} />
-	<Route path="/contact" component={Contact} />
-	</div>
-	
-	</BrowserRouter>
-    );
-  }
+	   componentDidMount() {    
+		document.title = `Testing title`;  
+	   }  
+	   
+	   componentDidUpdate() {    
+		document.title = `Testing title`;
+		}
+		
+		render() {  
+			return (
+			<BrowserRouter>
+			
+			<div  style={{
+				background: `url(${TopbarPhoto})`,
+				}}
+			>
+			<h3 style ={{color: "black"}}>Tommy To's personal website</h3>
+			<ul>
+			<p className = "p1"><Link to="/">Home</Link>&nbsp;
+			<Link to="/resume">Resume</Link>&nbsp;
+			<Link to="/project">Project</Link>&nbsp;
+			<Link to="/contact">Contact</Link>
+			</p>
+			</ul>
+			
+			<Route exact path="/" component={Home} />
+			<Route path="/resume" component={Resume} />
+			<Route path="/project" component={Project} />
+			<Route path="/contact" component={Contact} />
+			</div>
+			
+			</BrowserRouter>
+			);
+		}
   
 
 
